d721bb85b41add48fa55c3fd34ec36f46180b122
Jinesh Patel <jineshaws1990@gmail.com>
2022-01-09 16:45:55 -0500

    Space

--- TablesbyPython.py
+++ TablesbyPython.py
@@ -1,3 +1,3 @@
-num = int(input("Enter the number to get it's Table: "))
+num = int(input("Enter the number to get it's Table:  "))
 for i in range(1, 11):
     print(str(num) + " X " + str(i) + " = " + str(i*num))
